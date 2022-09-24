563be072762b8d465eda53e87f97ea495de54efd
Alex Kozadaev <akozadaev at yahoo com>
2022-09-24 22:22:53 +0100

    isPrime: remove concurrency as it was stalling

--- pkg/prime/prime.go
+++ pkg/prime/prime.go
@@ -7,16 +7,13 @@ import (
 	"io"
 	"log"
 	"math"
-	"sync"
-	"sync/atomic"
-	"time"
 )
 
 const errorMethod = "error"
 
 type Request struct {
-	Method string  `json:"method"`
-	Number float64 `json:"number"`
+	Method string   `json:"method"`
+	Number *float64 `json:"number"`
 }
 
 type Response struct {
@@ -24,14 +21,10 @@ type Response struct {
 	Prime  bool   `json:"prime"`
 }
 
-func getRequests(ctx context.Context, io io.ReadWriteCloser) <-chan Request {
-	defer io.Close()
-	ch := make(chan Request)
+func getLine(ctx context.Context, r io.Reader) <-chan []byte {
+	ch := make(chan []byte)
 
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	scanner := bufio.NewScanner(io)
+	scanner := bufio.NewScanner(r)
 	go func() {
 		defer close(ch)
 
@@ -49,102 +42,77 @@ func getRequests(ctx context.Context, io io.ReadWriteCloser) <-chan Request {
 				break
 			}
 
-			log.Println("handling input:", string(bytes))
-
-			var req Request
-			if err := json.Unmarshal(bytes, &req); err != nil || !validate(req) {
-				log.Printf("Invalid request - %s: %#v\n", err.Error(), req)
-				req.Method = errorMethod
-				ch <- req
-				return
-			}
-
-			ch <- req
+			ch <- bytes
 		}
 	}()
 
 	return ch
 }
 
-func Handle(ctx context.Context, ioh io.ReadWriteCloser) {
-	defer ioh.Close()
-
-	concur := make(chan struct{}, 5)
-	defer close(concur)
-
-	var (
-		cnt  int64
-		wg   sync.WaitGroup
-		resp []Response
-		enc  = json.NewEncoder(ioh)
-	)
-
-	for req := range getRequests(ctx, ioh) {
-		if req.Method == errorMethod {
-			_ = enc.Encode(req)
-			return
-		}
-
-		concur <- struct{}{}
-		resp = append(resp, Response{Method: "isPrime"})
-
-		log.Println("handling request:", req)
-
-		wg.Add(1)
-		go func(id int64, r Request) {
-			defer func() {
-				wg.Done()
-				<-concur
-			}()
-
-			select {
-			case <-ctx.Done():
-				log.Println("PrimeHandle: got canceled")
-				return
-
-			default:
-			}
-
-			// check if the number is integer
-			if r.Number != float64(int64(r.Number)) {
-				resp[id].Prime = false
-				return
-			}
-
-			resp[id].Prime = isPrime(int64(r.Number))
-		}(cnt, req)
-
-		atomic.AddInt64(&cnt, 1)
+func getResponse(req Request) Response {
+	if !validate(req) {
+		return Response{Method: errorMethod}
 	}
 
-	wg.Wait()
+	resp := Response{Method: "isPrime"}
+	num := *req.Number
 
-	for _, res := range resp {
-		if err := enc.Encode(res); err != nil {
-			log.Printf("Failed to encode response - %s: %v\n", err.Error(), res)
-			_ = enc.Encode(Response{Method: "error"})
+	// check if the number is integer
+	if num != float64(int64(num)) {
+		resp.Prime = false
+	}
+
+	resp.Prime = isPrime(int64(num))
+
+	return resp
+}
+
+func Handle(ctx context.Context, rw io.ReadWriter) {
+	enc := json.NewEncoder(rw)
+
+	for line := range getLine(ctx, rw) {
+		select {
+		case <-ctx.Done():
+			log.Println("PrimeHandle: got canceled")
+			return
+
+		default:
+		}
+
+		log.Println("handling input:", string(line))
+
+		var req Request
+		if err := json.Unmarshal(line, &req); err != nil {
+			log.Printf("Invalid request - %s\n", err.Error())
+			req.Method = errorMethod
+		}
+
+		resp := getResponse(req)
+
+		if err := enc.Encode(resp); err != nil {
+			log.Printf("Failed to encode response - %s: %v\n", err.Error(), resp)
+			_ = enc.Encode(Response{Method: errorMethod})
 			return
 		}
 
-		log.Println("Sent response:", res)
+		log.Println("Sent response:", resp)
 	}
 }
 
 // validate the request
-// A request is malformed if it is not a well-formed JSON object, if any
-// required field is missing, if the method name is not "isPrime", or if the
-// number value is not a number.
+// A request is malformed if it is not a well-formed JSON object, if any required field is
+// missing, if the method name is not "isPrime", or if the number value is not a number.
 func validate(req Request) bool {
-	return req.Method == "isPrime" // Unmarshal will check the number
+	return req.Method == "isPrime" && req.Number != nil
 }
 
 func isPrime(num int64) bool {
-	if num < 2 {
+	if num <= 1 {
 		return false
 	}
 
 	sqRoot := int64(math.Sqrt(float64(num)))
-	for i := int64(2); i < sqRoot; i++ {
+	for i := int64(2); i <= sqRoot; i++ {
 		if num%i == 0 {
 			return false
 		}
